9a185c352a1d65098997b09ecaa1f729a5360fb6
Andriy Diduh <damien_aron@yahoo.com>
2021-01-19 22:15:11 +0200

    Added completed test script.

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * This code test was completed by Andriy Diduh < andriy.work@yahoo.com >.
+ *
+ * @param {(number | (string | number)[])[]} inputArray - an array of the following format [[1, 2, 3, 4, 5], 3].
+ */
+
+// Group array element
+const groupArrayElements = (inputArray) => {
+  // Get the array, it should be the first element
+  const originalArray = inputArray[0];
+
+  // Get the "numberOfPieces" for number of pieces, should be the second element
+  const numberOfPieces = inputArray[1];
+
+  // Resulted array
+  const newGropedArray = [];
+
+  // First, get the number of items for each piece (for each sub array)
+  // - Match.floor rounds our number Down, for example 2.8 shall become 2
+  const amountForEachPiece = Math.floor(originalArray.length / numberOfPieces);
+
+  // For easy use we specify the Original Array length in a constant
+  const originalArrayLength = originalArray.length;
+
+  // Now we need to take the pieces from the original array and push it to the new one
+  // Reverse loops work faster so we will use a while loop, and looks nicer than a for loop
+  let loopIndex = numberOfPieces;
+  while (loopIndex--) {
+    if (loopIndex > 0) {
+      // Get the chucks of the originalArray and push to newGropedArray
+      newGropedArray.push(originalArray.splice(0, amountForEachPiece));
+    } else {
+      // Push the rest of the array, the remainder
+      newGropedArray.push(originalArray.splice(0, originalArrayLength));
+    }
+  }
+
+  return newGropedArray;
+};
+
+// TO TEST > put your array here and then re-run the script
+const inputArray = [
+  [4, 6, 3, 4, 37, 7, 8, 3, 8, 0, 9, 3, 49, 6, 46, 45, 6, 4, 56, 5],
+  6,
+];
+
+// Console log the results
+console.log(groupArrayElements(inputArray));
